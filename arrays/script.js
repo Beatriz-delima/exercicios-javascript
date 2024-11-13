7d60479e0d85063c214542e105c1e1961c3b15e3
Beatriz-delima <beatrizvieiradelima.ti@gmail.com>
2024-11-13 17:42:45 -0300

    add arrays folder

---
+++ arrays/script.js
@@ -0,0 +1,21 @@
+// CRIAR ARRAY
+let nomes
+nomes=["Bruna","Felipe","Fernando","Jessica"]
+
+// push -> adiciona 
+nomes.push("Luciene")
+
+// unshift -> adiciona ao final do array
+nomes.unshift("Francisco")
+
+//pop -> remove o último item do array
+nomes.pop()
+
+//shift -> remove o primeiro item do array 
+nomes.shift()
+
+//length -> informa o tamanho do array ( quantidade de itens)
+console.log(nomes.length)
+
+//indexOf -> retorna o índice de um item do array
+console.log(nomes.indexOf("Jessica"))
